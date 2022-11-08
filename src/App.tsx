37626c0625a83ecf2d720e39940dcfd5ddac3418
fkrein1 <felipekrein@gmail.com>
2022-11-08 16:54:26 -0300

    feat: router, default layout and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import ScrollToTop from './components/SrollToTop';
+import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Router />
+          <ScrollToTop />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import imagebg from '../../assets/header-bg.svg';
+
+export const HeaderContainer = styled.header`
+  height: 296px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${imagebg});
+  background-size: cover;
+  background-position: center;
+  img {
+    width: 148px;
+    height: 98ps;
+  }
+`;
